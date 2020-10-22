5647c6746625f845506687f3e0a876f1315ec271
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-10-21 20:51:02 -0600

    Initial commit

---
+++ src/main/java/com/example/springboottodolist/controller/TodoController.java
@@ -0,0 +1,46 @@
+package com.example.springboottodolist.controller;
+
+import com.example.springboottodolist.model.TodoItem;
+import com.example.springboottodolist.repo.TodoRepo;
+import com.sun.istack.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/todo")
+public class TodoController {
+
+    @Autowired
+    private TodoRepo todoRepo;
+
+    @GetMapping
+    public List<TodoItem> findAll(){
+        return todoRepo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Optional<TodoItem> findById(@PathVariable Long id){
+        return todoRepo.findById(id);
+    }
+
+    @PostMapping
+    public TodoItem save(@Valid @NotNull @RequestBody TodoItem todoItem){
+        return todoRepo.save(todoItem);
+    }
+
+    @PutMapping
+    public TodoItem update(@Valid @NotNull @RequestBody TodoItem todoItem){
+        return todoRepo.save(todoItem);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete(@PathVariable Long id){
+        todoRepo.deleteById(id);
+    }
+}
+
+
---
+++ src/test/java/com/example/springboottodolist/SpringboottodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
